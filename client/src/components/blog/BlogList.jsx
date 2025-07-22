6075ad60e0e7ab7ef6d2ab2ade7c28f9e099d833
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-07-22 11:09:40 +0530

    framer motion added for listing blog cards

--- client/src/components/blog/BlogList.jsx
+++ client/src/components/blog/BlogList.jsx
@@ -1,13 +1,28 @@
 import BlogCard from "./BlogCard";
+import { motion, AnimatePresence } from "framer-motion";
 
 const BlogList = ({ blogs }) => {
   if (!blogs.length) return <p>No blogs found.</p>;
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-6">
-      {blogs.map((blog) => (
-        <BlogCard key={blog._id} blog={blog} />
-      ))}
+      <AnimatePresence>
+        {blogs.map((blog, index) => (
+          <motion.div
+            key={blog._id}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{
+              duration: 0.4,
+              delay: index * 0.1,
+              ease: "easeOut",
+            }}
+          >
+            <BlogCard blog={blog} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
